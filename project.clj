1707e82598e550ebf4819ac5cbcd20a60ac31342
Sun Ning <sunng@about.me>
2014-07-05 06:08:17 +0800

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject info.sunng/debug "0.1.1"
+(defproject info.sunng/debug "0.1.2-SNAPSHOT"
   :description "Utility for clojure debug log"
   :url "http://github.com/sunng87/debug"
   :license {:name "Eclipse Public License"
